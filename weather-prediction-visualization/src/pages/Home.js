09d658546a1358162d32d6543c13c41b06938380
Adam Ma <adam.zym@gmail.com>
2025-04-12 15:27:05 -0400

    ...

--- weather-prediction-visualization/src/pages/Home.js
+++ weather-prediction-visualization/src/pages/Home.js
@@ -1,6 +1,6 @@
 export const Home = () => (
   <div>
-    <h1>Project Visualization Hub</h1>
+    <h1>Weather Prediction</h1>
     <p>
       We collected a Kaggle dataset of the hourly weather forecast in Boston, MA every day from the start of 2020 to the end of 2023.
       We used two machine learning methods, MLP and Time Series, to perform weather prediction.
